3e167d8f019e4f9baeb8be607ec64ad33ff99fd3
Nabil-Fareed Alikhan <nabil@happykhan.com>
2019-12-05 22:01:46 +0000

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,9 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `Happykhan.com`,
+    siteTitle: `Nabil-Fareed Alikhan`,
     siteTitleAlt: `Personal website of Nabil-Fareed Alikhan`,
+    siteUrl: `https://www.happykhan.com`,
     navigation: [
       {
         title: `Blog`,
@@ -50,9 +51,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Happykhan.com - Nabil-Fareed Alikhan`,
+        short_name: `happykhan.com`,
+        description: `Personal website of Nabil-Fareed Alikhan`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
