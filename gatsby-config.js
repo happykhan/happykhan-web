e789456d63477274bf527fd4b340f7b09a7f29ae
Nabil-Fareed Alikhan <nabil@happykhan.com>
2019-12-06 00:19:05 +0000

    2

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-34202032-1",
       },
     },
     `gatsby-plugin-sitemap`,
